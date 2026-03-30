f3685aab279d81f4b49b77ee4598e3e67ae9457f
neonheart'711 <amanmondal899@gmail.com>
2026-03-30 21:55:33 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
-const acc = document.querySelectorAll(".cource_botton");
+const acc = document.querySelectorAll(".course_botton");
 
-acc.forEach((btn) => {
-  btn.addEventListener("click", function () {
-    this.classList.toggle("active");
+acc.forEach(btn => {
+    btn.addEventListener("click", function () {
+        this.classList.toggle("active");
 
-    const panel = this.nextElementSibling;
+        const panel = this.nextElementSibling;
 
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    }
-  });
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
 });
